7fbd764ef8b4696dd7792f0a9b767bf98de513a0
Cristian Prigoana <cristian.prigoana@yahoo.com>
2016-01-24 22:47:59 +0100

    Working graph of top 250

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,83 @@
 
 function sayHello(){
     var id = document.getElementById("ID_input").value;
+    var actor_movies;
     $.get("getactor.php?id=123", function(data) {
-        alert("Data loaded: " + data);
+        //alert(data);
+
+        //alert(actor_movies);
+
+        console.log(data);
+        displayMainCanvas(data);
     });
+}
+
+
+function displayMainCanvas(actor_movies){
+     jsonFile =
+    '{ \
+        "nodes":[\
+        {"name":"node1","group":1},\
+        {"name":"node2","group":2},\
+        {"name":"node3","group":2},\
+        {"name":"node4","group":3}\
+    ],\
+        "links":[\
+        {"source":2,"target":1,"weight":1},\
+        {"source":0,"target":2,"weight":3}\
+    ]\
+    }';
+
+    jsonFile = actor_movies;
+
+    //console.log(jsonFile);
+    json = JSON.parse(jsonFile);
+    //json = actor_movies;
+    var width = 1600,
+        height = 900;
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var force = d3.layout.force()
+        .gravity(.05)
+        .distance(100)
+        .charge(-100)
+        .size([width, height]);
+    force
+        .nodes(json.nodes)
+        .links(json.links)
+        .start();
+
+    var link = svg.selectAll(".link")
+        .data(json.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+
+    var node = svg.selectAll(".node")
+        .data(json.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .call(force.drag);
+
+    node.append("circle")
+        .attr("r","5");
+
+    node.append("text")
+        .attr("dx", 12)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.name });
+
+    force.on("tick", function() {
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    });
+
+   // alert(movieJSON);
 }
--- main.php
+++ main.php
@@ -1,23 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+    <script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>
+
     <script src="/js/javascript.js" type="text/javascript"></script>
+
+    <style>
+
+        .link {
+            stroke: #aaa;
+        }
+
+        .node text {
+            stroke:#333;
+            cursos:pointer;
+        }
+
+        .node circle{
+            stroke:#fff;
+            stroke-width:3px;
+            fill:#555;
+        }
+
+    </style>
+
+
 </head>
 
 <body>
 
 <div id="mysql_info_div">
 
-<?php
-
-
-
-?>
 </div>
 Type ID:  <input type="text" name="ID_input" id="ID_input">
 <input type="button" onclick="sayHello()" value="Say it!">
 
+<div id="mainCanvas">
+</div>
 
 </body>
 </html>
